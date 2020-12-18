62d57333da3f7bb1b0a882ab388a2c50a587c4c2
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-17 18:36:40 -0800

    finished exam-end

--- ISFInal.py
+++ ISFInal.py
@@ -25,3 +25,27 @@ print percentage of grades above average
 
 
 """
+
+def main():
+    file = "Final.txt"
+    calculate_percent_above_average(file)
+
+def calculate_percent_above_average(file):
+    infile = open(file, 'r')
+    listGrades = [int(line.rstrip()) for line in infile]
+    infile.close()
+    length = len(listGrades)
+    sum1 =sum(listGrades)
+    avg = sum1 / length
+    print("Number of grades: ", length)
+    print("Average grade: ", avg)
+    counter = 0
+    for item in listGrades:
+        if item > avg:
+            counter += 1
+    percentHigher = counter / length 
+    print("Percentage of grades above average: ", end = " ")
+    print ("{0:.,2%}".format(percentHigher))
+
+
+main()
