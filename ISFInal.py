d2148e9b2e5d6d3c039e6e57a48b486e47129d57
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-17 18:21:16 -0800

    finished pseudo-code

--- ISFInal.py
+++ ISFInal.py
@@ -7,3 +7,21 @@ Then calculate the percent above average file
 Calculate the the percentage of grades that is greater than the average grades
 
 """
+
+"""
+open text file
+grades = infile input()
+Sum1 = sum of grades
+average = sum1 / grades 
+Print to user (Number of grades; grades)
+Print to user (Number of grades; average)
+Counter = 1
+    if the % of grades > average
+    Counter += 1
+% Higher  would be = counter/grades
+print number of grades to user 
+print average grade 
+print percentage of grades above average
+
+
+"""
