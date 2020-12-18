0e59e1e6df16a6cbfbc6eeeab1f8c2c9e66380b5
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-17 18:13:28 -0800

    add changes

---
+++ ISFInal.py
@@ -0,0 +1,9 @@
+
+
+"""
+This program is to display the number of grades of final exam
+Upload file of grade
+Then calculate the percent above average file
+Calculate the the percentage of grades that is greater than the average grades
+
+"""
